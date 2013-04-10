da0a7a577565c5da0b6264b3c85c355d6b314fc8
Morten Fangel <fangel@sevengoslings.net>
2013-04-10 19:06:31 +0200

    Finished proof for insert_preserves_sort.

--- code/bpt_proofs.v
+++ code/bpt_proofs.v
@@ -16,60 +16,55 @@ Proof.
   apply kvl_sorted_cons. apply H2. apply H4.
 Qed.
 
+Lemma list_tail_is_sorted : forall (X: Type) (l: list (nat * X)) (k: nat) (v: X),
+  kvl_sorted ((k,v)::l) -> kvl_sorted l.
+Proof.
+  intros.
+  inversion H. apply kvl_sorted_0. apply H2.
+Qed.
+
 Theorem insert_preserves_sort : forall (X: Type) (l: list (nat * X)) (k: nat) (v: X),
   kvl_sorted l -> kvl_sorted (insert_into_list k v l).
 Proof.
   intros. induction H.
   Case "kvl_sorted_0". simpl. apply kvl_sorted_1.
-  Case "kvl_sorted_1". simpl. remember (ble_nat k n) as klen. destruct klen.
-    SCase "k <= n". remember (beq_nat k n) as keqn. destruct keqn.
+  Case "kvl_sorted_1". simpl. 
+    remember (ble_nat k n) as klen. 
+    destruct klen; symmetry in Heqklen; [apply ble_nat_true in Heqklen | apply ble_nat_false in Heqklen].
+    SCase "k <= n". remember (beq_nat k n) as keqn. 
+      destruct keqn; symmetry in Heqkeqn; [apply beq_nat_true_iff in Heqkeqn | apply beq_nat_false_iff in Heqkeqn].
       SSCase "k = n". apply kvl_sorted_1.
       SSCase "k != n". apply kvl_sorted_cons. apply kvl_sorted_1.
-      				   symmetry in Heqklen. apply ble_nat_true in Heqklen. 
-                       symmetry in Heqkeqn. apply beq_nat_false_iff in Heqkeqn.
                        apply blt_nat_true. omega.
     SCase "k > n". apply kvl_sorted_cons. apply kvl_sorted_1.
-    	symmetry in Heqklen. apply ble_nat_false in Heqklen.
     	apply blt_nat_true. omega.
-  Case "kvl_sorted_cons". 
-    simpl. remember (ble_nat k n1) as klen1. destruct klen1.
-    SCase "k <= n1". remember (beq_nat k n1) as keqn1. destruct keqn1.
-      apply kvl_sorted_cons. apply H. apply blt_nat_true in H0.
-      				   symmetry in Heqklen1. apply ble_nat_true in Heqklen1. 
-                       symmetry in Heqkeqn1. apply beq_nat_true_iff in Heqkeqn1.
-                       apply blt_nat_true. omega.
-      apply kvl_sorted_cons. apply kvl_sorted_cons. apply H. apply H0.
-         apply blt_nat_true in H0.
-         symmetry in Heqklen1. apply ble_nat_true in Heqklen1. 
-         symmetry in Heqkeqn1. apply beq_nat_false_iff in Heqkeqn1.
-         apply blt_nat_true. omega.
-    SCase "k > n1".  remember (ble_nat k n2) as klen2. destruct klen2.
-      SSCase "k <= n2". remember (beq_nat k n2) as keqn2. destruct keqn2.
+  Case "kvl_sorted_cons".
+    apply blt_nat_true in H0.
+    simpl. remember (ble_nat k n1) as klen1.
+    destruct klen1; symmetry in Heqklen1; [apply ble_nat_true in Heqklen1 | apply ble_nat_false in Heqklen1].
+    SCase "k <= n1". remember (beq_nat k n1) as keqn1.
+      destruct keqn1; symmetry in Heqkeqn1; [apply beq_nat_true_iff in Heqkeqn1 | apply beq_nat_false_iff in Heqkeqn1].
+      SSCase "k = n1".
+        apply kvl_sorted_cons. apply H. apply blt_nat_true. omega.
+      SSCase "k != n1".
+        apply kvl_sorted_cons. apply kvl_sorted_cons. apply H. apply blt_nat_true. apply H0.
+        apply blt_nat_true. omega.
+    SCase "k > n1". simpl in IHkvl_sorted. remember (ble_nat k n2) as klen2. 
+      destruct klen2; symmetry in Heqklen2; [apply ble_nat_true in Heqklen2 | apply ble_nat_false in Heqklen2].
+      SSCase "k <= n2". remember (beq_nat k n2) as keqn2.
+        destruct keqn2; symmetry in Heqkeqn2; [apply beq_nat_true_iff in Heqkeqn2 | apply beq_nat_false_iff in Heqkeqn2].
         SSSCase "k = n2".
-          apply kvl_sorted_cons. apply blt_nat_true in H0. 
-          symmetry in Heqklen1. apply ble_nat_false in Heqklen1.
-          symmetry in Heqklen2. apply ble_nat_true in Heqklen2.
-          symmetry in Heqkeqn2. apply beq_nat_true_iff in Heqkeqn2. subst.
-          eapply sort_ignores_value. apply H. apply blt_nat_true in H0.
-          symmetry in Heqklen1. apply ble_nat_false in Heqklen1.
-          symmetry in Heqklen2. apply ble_nat_true in Heqklen2.
-          symmetry in Heqkeqn2. apply beq_nat_true_iff in Heqkeqn2.
+          apply kvl_sorted_cons. subst.
+          eapply sort_ignores_value. apply H.
           apply blt_nat_true. omega.
         SSSCase "k != n2". 
           apply kvl_sorted_cons. apply kvl_sorted_cons. apply H.
-          apply blt_nat_true in H0. 
-          symmetry in Heqklen1. apply ble_nat_false in Heqklen1.
-          symmetry in Heqklen2. apply ble_nat_true in Heqklen2.
-          symmetry in Heqkeqn2. apply beq_nat_false_iff in Heqkeqn2.
           apply blt_nat_true. omega.
-          apply blt_nat_true in H0. 
-          symmetry in Heqklen1. apply ble_nat_false in Heqklen1.
-          symmetry in Heqklen2. apply ble_nat_true in Heqklen2.
-          symmetry in Heqkeqn2. apply beq_nat_false_iff in Heqkeqn2.
           apply blt_nat_true. omega.
-     SSCase "k > n2".
-       admit.
-Admitted.
+     SSCase "k > n2". 
+       apply kvl_sorted_cons. apply IHkvl_sorted.
+       apply blt_nat_true. omega.
+Qed.
 
 Theorem insert_works : forall (X: Type) (l: list (nat * X)) (k: nat) (v: X),
   search_leaf k (insert_into_list k v l) = Some v. 
