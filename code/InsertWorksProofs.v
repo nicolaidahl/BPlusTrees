3b383b6f0f1033a8e5914f86d593d9c8f7f7e8e8
Christian Harrington <christian@harrington.dk>
2013-04-22 13:45:48 +0200

    Initial work on split_insert_left theorem

--- code/InsertWorksProofs.v
+++ code/InsertWorksProofs.v
@@ -1,6 +1,15 @@
 Require Export BPlusTree.
 Require Export HelperProofs.
 Require Export SortingProofs.
+Require Export HelperFunctions.
+
+Inductive appears_in_kvl {X:Type} (sk: nat) : list (nat * X) -> Prop :=
+  | ai_here : forall v l, appears_in_kvl sk ((sk, v)::l)
+  | ai_later : forall skb v l, appears_in_kvl sk l -> appears_in_kvl sk ((skb, v)::l).
+  
+(*Theorem split_insert_left : forall (X: Type) (b: nat) (kvl: list (nat * X)) (k k1 kb: nat) (v vb: X) (leaf: list (nat * X)),
+  kvl_sorted kvl -> not (appears_in_kvl k kvl) -> element_at_index b kvl = Some (kb, vb) -> k < kb ->
+  (length kvl) = (mult b 2) -> insert_leaf b k v leaf = bptNode b X left [(k1, right)] /\ appears_in_kvl k left.*)
 
 Theorem insert_into_list_works : forall (X: Type) (l: list (nat * X)) (k: nat) (v: X),
   kvl_sorted l -> search_leaf k (insert_into_list k v l) = Some v. 
@@ -35,3 +44,12 @@ Theorem insert_leaf_split : forall (X: Type) (b: nat) (k: nat) (v: X) (kvl kvl1 
 Proof.
   admit.
 Admitted.
+
+Theorem insert_works : forall (b: nat) (X: Type) (t: bplustree b X) (k: nat) (v: X),
+  valid_bplustree b X t -> search k (insert k v t) = Some v.
+Proof.
+  intros.
+  induction H.
+  Case "leaf".
+    unfold insert. remember (insert' k v (bptLeaf b X l)) as insert'. destruct insert'.
+    
