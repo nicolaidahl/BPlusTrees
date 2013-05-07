5e2cc0ea775f97dc5fb0efc50131812e343e7faa
Morten Fangel <fangel@sevengoslings.net>
2013-05-07 11:00:08 +0200

    Proved insert_not_split_impl_space_left (and helper-lemma insert_new_into_list_length_gt_length).

--- code/InsertWorksProofs.v
+++ code/InsertWorksProofs.v
@@ -129,6 +129,30 @@ Proof.
       simpl in H1. omega.
 Qed.
 
+Lemma insert_new_into_list_length_gt_length : forall (X: Type) (k: nat) (v: X) (l: list (nat*X)),
+  ~ appears_in_kvl k l ->
+  length l < length (insert_into_list k v l).
+Proof.
+  intros.
+  induction l.
+  Case "l = []".
+    simpl. omega.
+  Case "l = a::l". destruct a.
+    simpl. remember (ble_nat k n) as klen.
+    destruct klen; symmetry in Heqklen; [apply ble_nat_true in Heqklen | apply ble_nat_false in Heqklen].
+    SCase "k <= n".
+      remember (beq_nat k n) as keqn.
+      destruct keqn; symmetry in Heqkeqn; [apply beq_nat_true_iff in Heqkeqn | apply beq_nat_false_iff in Heqkeqn].
+      SSCase "k = n".
+        rewrite Heqkeqn in H.
+        apply ex_falso_quodlibet. apply H. apply ai_here.
+      SSCase "k < n".
+        simpl. omega.
+    SCase "k > n".
+      simpl.
+      apply n_lt_m__Sn_lt_Sm.
+      apply IHl. unfold not. intro. apply H. apply ai_later. apply H0.
+Qed.
 
 Lemma insert_leaf_cons_eq : forall (X: Type) (b k1 k2: nat) (v1 v2: X) (l: list (nat * X)),
   b <> 0 -> kvl_sorted ((k2, v2)::l) -> k1 = k2 -> length((k2,v2)::l) <= mult b 2 -> insert_leaf b k1 v1 ((k2, v2) :: l) = ((k1,v1)::l, None).
@@ -837,13 +861,21 @@ Proof.
   eapply appears_in_super_set_appears. reflexivity. apply H.
 Qed.
 
+
 Lemma insert_not_split_impl_space_left: forall {X: Type} (b: nat) k (v:X) l l',
   ~ appears_in_kvl k l ->
   insert_leaf b k v l = (l', None) -> length l < b * 2.
 Proof.
-  intros. unfold insert_leaf in H. remember (ble_nat (length (insert_into_list k v l)) (b * 2)) as blelen.
-  destruct blelen. Admitted.
-
+  intros.
+  unfold insert_leaf in H0.
+  remember (ble_nat (length (insert_into_list k v l)) (b * 2)) as blelen.
+  destruct blelen.
+  symmetry in Heqblelen. apply ble_nat_true in Heqblelen.
+  assert (length l < length (insert_into_list k v l)).
+    apply insert_new_into_list_length_gt_length; assumption.
+  omega.
+  inversion H0.
+Qed.
 
 
 Lemma appears_in_split_node_appears_in_lists: forall {X: Type} b k n left right, 
