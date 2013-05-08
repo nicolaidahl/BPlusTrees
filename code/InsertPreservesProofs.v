a271c714fd07cb12405918a7616d9fb1ac112295
Nicolai Dahl <ndahl89@gmail.com>
2013-05-08 19:49:05 +0200

    The whole project compiles again

--- code/InsertPreservesProofs.v
+++ code/InsertPreservesProofs.v
@@ -4,13 +4,13 @@ Require Export SortingProofs.
 Require Export ValidBPlusTree.
 Require Export InsertProofs.
 
-Lemma insert_into_node_impl_leaf_false: forall {X: Type} b k v l l' o,
-  insert' k v (bptNode b X l) = (bptLeaf b X l', o) -> False.
+Lemma insert_into_node_impl_leaf_false: forall {X: Type} (b k counter: nat) (v: X) l l' (o: option (nat * bplustree b X)),
+  insert' counter k v (bptNode b X l) = (bptLeaf b X l', o) -> False.
 Proof. Admitted.
 
-Lemma insert_leaf_preserves_min_fan_out : forall {X: Type} b k v o l l', 
+Lemma insert_leaf_preserves_min_fan_out : forall {X: Type} b k v o counter l l', 
   valid_sub_bplustree b X (bptLeaf b X l) ->
-  insert' k v (bptLeaf b X l) = (bptLeaf b X l', o) ->
+  insert' counter k v (bptLeaf b X l) = (bptLeaf b X l', o) ->
   length l' >= b.
 Proof.
   intros. induction H.
@@ -24,13 +24,13 @@ Lemma insert_into_list_max_grows_one: forall {X: Type} k (v: X) l,
   length (insert_into_list k v l) = length l \/ length (insert_into_list k v l) = S (length l).
 Proof. Admitted.
 
-Lemma insert_leaf_split_preserves_max_fan_out : forall {X: Type} b k v t n l l', 
-  (t, Some (n, bptLeaf b X l')) = insert' k v (bptLeaf b X l) ->
+Lemma insert_leaf_split_preserves_max_fan_out : forall {X: Type} b counter k v t n l l', 
+  (t, Some (n, bptLeaf b X l')) = insert' counter k v (bptLeaf b X l) ->
   length l' <= b * 2.
 Proof. Admitted.
 
-Lemma insert_leaf_preserves_max_fan_out : forall {X: Type} b k v o l l', 
-  (bptLeaf b X l', o) = insert' k v (bptLeaf b X l) ->
+Lemma insert_leaf_preserves_max_fan_out : forall {X: Type} b counter k v o l l', 
+  (bptLeaf b X l', o) = insert' counter k v (bptLeaf b X l) ->
   length l' <= b * 2.
 Proof.
   intros. unfold insert' in H. unfold insert_leaf in H.
@@ -39,47 +39,45 @@ Proof.
   Case "blelen true".
     apply ble_nat_true in Heqblelen. inversion H.
     assert (length (insert_into_list k v l) = length l \/ length (insert_into_list k v l) = S (length l)).
-    apply insert_into_list_max_grows_one. destruct H0. assumption. 
-    assumption.
-  Case "blelen false".
-    apply ble_nat_false in Heqblelen.     
-     admit.
+    apply insert_into_list_max_grows_one. destruct H0. admit. admit.
+    
+    admit.
 Admitted.
   
   
 
-Lemma insert_leaf_preserves_sort : forall {X: Type} b k v o l l', 
-  kvl_sorted l -> (bptLeaf b X l', o) = insert' k v (bptLeaf b X l) ->
+Lemma insert_leaf_preserves_sort : forall {X: Type} b counter k v o l l', 
+  kvl_sorted l -> (bptLeaf b X l', o) = insert' counter k v (bptLeaf b X l) ->
   kvl_sorted l'.
 Proof. Admitted.
 
-Lemma insert_leaf_split_preserves_min_fan_out : forall {X: Type} b k v t n l l', 
-  (t, Some (n, bptLeaf b X l')) = insert' k v (bptLeaf b X l) ->
+Lemma insert_leaf_split_preserves_min_fan_out : forall {X: Type} b counter k v t n l l', 
+  (t, Some (n, bptLeaf b X l')) = insert' counter k v (bptLeaf b X l) ->
   length l' >= b.
 Proof. Admitted.
 
 
 
-Lemma insert_leaf_split_preserves_sort_right : forall {X: Type} b k v t n l l', 
-  kvl_sorted l -> (t, Some (n, bptLeaf b X l')) = insert' k v (bptLeaf b X l) ->
+Lemma insert_leaf_split_preserves_sort_right : forall {X: Type} b counter k v t n l l', 
+  kvl_sorted l -> (t, Some (n, bptLeaf b X l')) = insert' counter k v (bptLeaf b X l) ->
   kvl_sorted l'.
 Proof. Admitted.
 
 
-Lemma insert_leaf_split_never_node : forall {X: Type} b k v t o n l l', 
-  ((t, Some (n, bptNode b X l')) = insert' k v (bptLeaf b X l) \/
-  (bptNode b X l', o) = insert' k v (bptLeaf b X l)) ->
+Lemma insert_leaf_split_never_node : forall {X: Type} b counter k v t o n l l', 
+  ((t, Some (n, bptNode b X l')) = insert' counter k v (bptLeaf b X l) \/
+  (bptNode b X l', o) = insert' counter k v (bptLeaf b X l)) ->
   False.
 Proof. Admitted.
 
-Lemma insert_leaf_split_ensures_n_positive : forall {X: Type} b k v n t l l',
-  kvl_sorted l -> (t, Some (n, l')) = insert' k v (bptLeaf b X l) ->
+Lemma insert_leaf_split_ensures_n_positive : forall {X: Type} b counter k v n t l l',
+  kvl_sorted l -> (t, Some (n, l')) = insert' counter k v (bptLeaf b X l) ->
   n > 0.
 Proof.
 Admitted.
 
-Lemma split_leaf_impl_length_max_fan_out: forall {X: Type} b t1 t2 n k v l,
-   (t1, Some (n, t2)) = insert' k v (bptLeaf b X l) ->
+Lemma split_leaf_impl_length_max_fan_out: forall {X: Type} b counter t1 t2 n k v l,
+   (t1, Some (n, t2)) = insert' counter k v (bptLeaf b X l) ->
    length l = S (b * 2).
 Proof. Admitted.
 
@@ -88,7 +86,7 @@ Theorem insert_preserves_valid_bplustree : forall (b: nat) (X: Type) (tree: bplu
 Proof.
   intros. induction H.
   Case "root_is_a_leaf".
-    unfold insert. remember (insert' k v (bptLeaf b X l)) as insert'. destruct insert'. destruct o.
+    unfold insert. remember (insert' (height (bptLeaf b X l)) k v (bptLeaf b X l)) as insert'. destruct insert'. destruct o.
     SCase "o = Some(key, right)".
       destruct p. apply valid_root_node; try assumption; try (simpl; omega). 
       SSCase "all_values valid subtree".
