73452a74181be0b674a990b3a5a35254219b6dda
Nicolai Dahl <ndahl89@gmail.com>
2013-05-09 20:30:35 +0200

    Altered the background section a bit

--- code/InsertProofs.v
+++ code/InsertProofs.v
@@ -531,3 +531,50 @@ Proof.
   intros. unfold key_at_index in H. destruct l. reflexivity. destruct p. inversion H.
 Qed.
 
+Theorem insert_leaf_split_never_empty: forall {X: Type} b k (v: X) l l1 l2,
+  b <> 0 ->
+  (l1, Some l2) = insert_leaf b k v l -> l1 <> [] /\ l2 <> [].
+Proof. 
+  intros.
+  destruct l.
+  Case "l = []".
+    unfold insert_leaf in H0.
+    simpl in H0. remember (b*2). destruct n.
+    apply ex_falso_quodlibet. omega.
+    inversion H0.
+  Case "l = p::l". destruct p.
+    unfold insert_leaf in H0.
+    simpl in H0.
+    remember (ble_nat k n) as klen. 
+    destruct klen; symmetry in Heqklen; [apply ble_nat_true in Heqklen | apply ble_nat_false in Heqklen].
+    SCase "k <= n". remember (beq_nat k n) as keqn. 
+      destruct keqn; symmetry in Heqkeqn; [apply beq_nat_true_iff in Heqkeqn | apply beq_nat_false_iff in Heqkeqn].
+      SSCase "k = n".
+        simpl in H0. replace (b*2) with (S(S(pred(b)*2))) in H0 by omega.
+        remember (ble_nat (length l) (S (pred b * 2))) as tl.
+        destruct tl. inversion H0.
+        inversion H0.
+        symmetry in Heqtl. apply ble_nat_false in Heqtl.
+        split.
+          apply cut_left_not_nil; simpl; try assumption; try omega.
+          apply cut_right_not_nil; simpl; try assumption; try omega.
+      SSCase "k < n".
+        remember (ble_nat (length ((k, v) :: (n, x) :: l)) (b * 2)) as tl.
+        destruct tl. inversion H0.
+        symmetry in Heqtl. apply ble_nat_false in Heqtl. simpl in Heqtl.
+        inversion H0.
+        split.
+          apply cut_left_not_nil; simpl; try assumption; try omega. 
+          apply cut_right_not_nil; simpl; try assumption; try omega.
+    SCase "k > n".
+      remember (ble_nat (length ((n, x) :: insert_into_list k v l)) (b * 2)) as tl.
+      destruct tl. inversion H0.
+      inversion H0.
+      symmetry in Heqtl. apply ble_nat_false in Heqtl. simpl in Heqtl.
+      split.
+          apply cut_left_not_nil; simpl; try assumption; try omega. 
+          apply cut_right_not_nil; simpl; try assumption; try omega.
+Qed.
+
+
+
